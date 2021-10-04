329f08f16c7ba17762a9fd9de235a1b044162253
Zak003 <59819711+Zak003@users.noreply.github.com>
2021-10-04 09:02:47 +0200

    Popravek css za koledar

--- index2.php
+++ index2.php
@@ -40,25 +40,25 @@ function whileSQL($con, $day, $time){
   $c = true;
   while($row=mysqli_fetch_assoc($result)){
     $c = false;
-    $cas = new DateTime(date("Y-m-d",$day)." $time:00");
+    $cas = new DateTime(date("Y-m-d",$day)." $time:30");
     $zacetek = new DateTime($row["start_time"]);
     $konec = new DateTime($row["end_time"]);
     if (($cas > $zacetek) && ($cas <= $konec)){
       if($row['room_id'] == 1)
       {
-          echo "<div class='event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Reserved by: ".$row["reservation_name"]."</span>"."</div>"."</div>";
+          echo "<div class='event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Name of reservation: ".$row["reservation_name"]."</span>"."</div>"."</div>";
       }
       else if($row['room_id'] == 3)
       {
-        echo "<div class='drugi_event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Reserved by: ".$row["reservation_name"]."</span>"."</div>"."</div>";
+        echo "<div class='drugi_event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Name of reservation: ".$row["reservation_name"]."</span>"."</div>"."</div>";
       }
       else if($row['room_id'] == 5)
       {
-        echo "<div class='tretji_event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Reserved by: ".$row["reservation_name"]."</span>"."</div>"."</div>";
+        echo "<div class='tretji_event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Name of reservation: ".$row["reservation_name"]."</span>"."</div>"."</div>";
       }
       else
       {
-        echo "<div class='cetrti_event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Reserved by: ".$row["reservation_name"]."</span>"."</div>"."</div>";
+        echo "<div class='cetrti_event'>".$row["room_name"]."<div class='overlay_event'>"."<span class='text_overlay'>"."Name of reservation: ".$row["reservation_name"]."</span>"."</div>"."</div>";
       }
     }
     else{
@@ -146,12 +146,7 @@ function whileSQL($con, $day, $time){
         ?>
 
       </div>
-      <?php echo '<a href="rezervacija.php?ajdi_sobe='.$room_id.'" class="overlay_povezava"><p class="text_overlay">
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
-          <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
-          <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
-        </svg>
-          Book for '.$room_name.'</p></a>';?> 
+      <?php echo '<a href="rezervacija.php?ajdi_sobe='.$room_id.'" class="overlay_povezava">Book for '.$room_name.'</a>';?> 
       </div>
                           
     </div><hr class="crta_pod_sliko"><?php } ?>
